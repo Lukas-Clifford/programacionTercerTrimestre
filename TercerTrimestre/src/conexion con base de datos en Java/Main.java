7b3038b978e10c75690f87ebdef4d60ba7f5a6ee
Lukas Clifford <lukas.vogdt@gmail.com>
2025-04-23 08:59:47 +0200

    Add files via upload

---
+++ TercerTrimestre/src/conexion con base de datos en Java/Main.java
@@ -0,0 +1,105 @@
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Controller controller = new Controller("root","");
+
+
+//        insertValues(controller);
+
+
+//        update(controller);
+
+
+//        deleteRoute(controller);
+
+
+        querys(controller);
+
+
+        controller.closeConnection();
+
+    }
+
+    private static void querys(Controller controller) {
+        try {
+
+            int numdriver = 111;
+            System.out.println("Driver data from numdriver : " + numdriver);
+
+            controller.executeSelectQuery(
+                "SELECT numdriver, name, surname " +
+                    "FROM driver " +
+                    "WHERE numdriver = " + numdriver
+            );
+
+            System.out.println();
+
+
+            String city = "'Estepona'";
+            System.out.println("Day of the week from city : " + city);
+
+            controller.executeSelectQuery(
+                "SELECT day_of_week " +
+                    "FROM bdp " +
+                    "JOIN place USING(idplace) " +
+                    "WHERE place.city = " + city
+            );
+
+            System.out.println();
+
+
+            String register = "'B002'";
+            System.out.println("Drivers that drive bus with register: " + register);
+
+            controller.executeSelectQuery(
+                "SELECT numdriver, name, surname " +
+                    "FROM driver " +
+                    "JOIN bdp USING (numdriver) " +
+                    "JOIN bus USING (register) " +
+                    "WHERE register = " + register
+            );
+
+
+
+
+
+        } catch (Controller.NotSelectQuery e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private static void deleteRoute(Controller controller) {
+
+        controller.deleteRoute("B009",109,9);
+
+    }
+
+
+
+    private static void update(Controller controller) {
+
+        controller.updateDayOfWeek(111,"Monday");
+
+    }
+
+
+
+    private static void insertValues(Controller controller) {
+
+        controller.insertBus("B012","Escolar", "LIC011");
+
+        controller.insertDriver(111, "Arnold","Schwarzenegger");
+
+        controller.insertPlace(11,"29680", "Estepona", "Reloj");
+
+        controller.insertRoute("B012", 111, 11, "Sunday");
+
+    }
+
+
+}
